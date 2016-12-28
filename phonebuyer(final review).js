b06525da9be6a37d69c4d77c04d723bf1fb1fdc4
YamiPR <thatblueeyesguy@gmail.com>
2016-12-28 01:25:32 -0400

    Add files via upload
    Write a program to calculate the total price of your phone purchase. You will keep purchasing phones (hint: loop!) until you run out of money in your bank account. You'll also buy accessories for each phone as long as your purchase amount is below your mental spending threshold.
    After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted.
    Finally, check the amount against your bank account balance to see if you can afford it or not.
    You should set up some constants for the "tax rate," "phone price," "accessory price," and "spending threshold," as well as a variable for your "bank account balance.""
    You should define functions for calculating the tax and for formatting the price with a "$" and rounding to two decimal places.

---
+++ phonebuyer(final review).js
@@ -0,0 +1,40 @@
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESORY_PRICE = 9.99;
+const SPENDING_THRESHOLD = 200.00;
+
+var accountbalance = 303.91;
+var phonecount = 0;
+var accesoriescount = 0;
+var spentatm = 0;
+
+function calculateTax( amt ) {
+	amt = amt + ( amt * TAX_RATE );
+	
+	return amt;
+}
+
+function formatPrice( amt ) {
+	return "$" + amt.toFixed( 2 );
+}
+
+while (spentatm < accountbalance ) {
+	
+	spentatm += PHONE_PRICE;
+	phonecount++;
+	
+	if ( spentatm < SPENDING_THRESHOLD ) {
+		spentatm += ACCESORY_PRICE;
+		accesoriescount++;
+	}
+}
+
+console.log("Bought " + phonecount + " phones and " + accesoriescount + " phone accesories. Total cost is " + formatPrice(calculateTax(spentatm)));
+
+if ( spentatm < accountbalance ){
+	console.log( "You can afford buying all of this." );
+}
+
+else ( spentatm > accountbalance ){
+	console.log( "You can't afford buying all of this." );
+}
